0db6a4e5d847268f85c68c2b303866e8db746a60
ilaydadastan <ilaydastan2@gmail.com>
2025-05-04 21:50:02 +0300

    fix profile usage

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from app.api import api_bp
 from config import config_by_profile_name
@@ -5,7 +7,9 @@ from app.initialization import db, ma, migrate
 from flask_cors import CORS
 
 
-def create_app(profile_name):
+def create_app():
+    profile_name = os.getenv("ACTIVE_PROFILE", "development")
+
     app = Flask(__name__)
 
     app.config.from_object(config_by_profile_name[profile_name])
