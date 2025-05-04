d5a2ac47911c61d259ca9c0802d34170014e4788
ilaydadastan <ilaydastan2@gmail.com>
2025-05-04 20:58:33 +0300

    add VITE_REACT_APP_URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 8080,
-        strictPort: true,
-        host: true,
-        origin: "http://0.0.0.0:8080",
-    },
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 8080,
+            strictPort: true,
+            host: true,
+            origin: env.VITE_REACT_APP_URL,
+        },
+    }
 })
